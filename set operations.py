5fe9a4d5f54539bc33153c4e2fd7ace025f808f3
manaswibutle <manaswi.butle.batch2025@sitnagpur.siu.edu.in>
2026-02-05 15:19:44 +0530

    Update set operations.py

--- set operations.py
+++ set operations.py
@@ -1,5 +1,5 @@
 set_a = set(map(int, input("Set A: ").split()))
-set_b = set(map(int, input(Set B: ").SPLIY()))
+set_b = set(map(int, input("Set B: ").split()))
 union_set = set_a | set b
 intersection_set = set_a & set_b
 difference_set = set_a - set_b
@@ -7,3 +7,4 @@ difference_set = set_a - set_b
 print("Union: ", union_set)
 print("Intersection: ", intersection_set)
 print("Difference: ", difference_set) 
+
