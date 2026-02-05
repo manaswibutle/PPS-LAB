7553fe0dd77707627d8072d69c2049fd270936b8
manaswibutle <manaswi.butle.batch2025@sitnagpur.siu.edu.in>
2026-02-05 14:45:35 +0530

    Add files via upload

---
+++ set operations.py
@@ -0,0 +1,9 @@
+set_a = set(map(int, input("Set A: ").split()))
+set_b = set(map(int, input(Set B: ").SPLIY()))
+union_set = set_a | set b
+intersection_set = set_a & set_b
+difference_set = set_a - set_b
+
+print("Union: ", union_set)
+print("Intersection: ", intersection_set)
+print("Difference: ", difference_set) 
